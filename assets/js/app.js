cf3138e2c401c319f7ff881c2d6fe9751cb234c2
Noor-Abdelmuneim <157888524+Noor-Abdelmuneim@users.noreply.github.com>
2024-04-03 13:33:48 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => { // corrected the syntax here
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('tab__active');
+        });
+
+        target.classList.add('tab__active');
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab__active')
+        });
+
+        tab.classList.add('tab__active')
+    });
+});
+
+
+// show menu
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+    navToggle.addEventListener('click' , () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if(navClose) {
+    navClose.addEventListener('click' , () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+
+// removemenu
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu'); 
+
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
